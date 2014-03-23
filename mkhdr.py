1c09ccdbdf3e3bbeaa22a4899cd0ad798c4143b2
Tianming.Lu <lutianming1005@gmail.com>
2014-03-23 12:24:02 +0100

    implement reinchards global operator. use numpy array to improve speed

--- mkhdr.py
+++ mkhdr.py
@@ -1,6 +1,8 @@
 import os
 import sys
 from PIL import Image
+from skimage import filter
+import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -20,7 +22,7 @@ def read_images(filenames):
 
     # sort by image exposure time
     images.sort(key=exposure_time)
-    times = [exposure_time(im) for im in images]
+    times = np.array([exposure_time(im) for im in images])
     images = [np.array(im, dtype=np.int) for im in images]
     return images, times
 
@@ -43,7 +45,7 @@ def recover_g(imgs, times, index_x, index_y):
     for i, im in enumerate(imgs):
         Z[:, i] = np.array(im[index_x, index_y], dtype=np.int).flatten()
 
-    l = 1000
+    l = 100
     g, lE = solve_g(Z, B, l, weight_function)
     return g
 
@@ -90,7 +92,7 @@ def weight_function(z):
 
 
 def gen_weight_map():
-    w = [weight_function(z) for z in range(256)]
+    w = np.array([weight_function(z) for z in range(256)])
     return w
 
 
@@ -103,26 +105,47 @@ def radiance(g, imgs, times, w):
     for i, im in enumerate(imgs):
         pixels[:, i] = im.flatten()
 
-    # for z in range(256):
-    #     rows, cols = np.where(pixels == z)
-    #     tmp[rows, cols] = g[z]
-    #     weight[rows, cols] = w(z)
-    # tmp = tmp - np.log(times)
-
-    vfun = np.vectorize(lambda z: g[z])
-    tmp = vfun(pixels)
-    tmp = tmp - np.log(times)
-
-    vfun = np.vectorize(lambda z: w[z])
-    weight = vfun(pixels)
+    tmp = g[pixels] - np.log(times)
+    weight = w[pixels]
+    weight[pixels == 255] = 0
 
     lnE = np.sum(weight*tmp, axis=1) / np.sum(weight, axis=1)
-    return lnE.reshape((width, height))
+    return np.exp(lnE.reshape((width, height)))
 
 
-def tone_mapping(E):
+def globals_simple(E):
     p = E / (E+1) * 255
     return p
+
+
+def local_tone_mapping(R, G, B, sigma_r=0.4, sigma_d=100):
+    intensity = 0.2989*R + 0.5866*G + 0.1145*B
+    r = R / intensity
+    g = G / intensity
+    b = B / intensity
+    log_in_intensity = np.log(intensity).astype(np.float32)
+    # log_base = filter.denoise_bilateral(log_in_intensity, sigma_r, sigma_d)
+    log_base = cv2.bilateralFilter(log_in_intensity, 5, 50, 50)
+    log_detail = log_in_intensity - log_base
+
+    compressFactor = np.log(50) / (np.max(log_base) - np.min(log_base))
+    scaleFactor = np.max(log_base)*compressFactor
+
+    log_out_intensity = log_base*compressFactor - scaleFactor + log_detail
+    R = r * np.exp(log_out_intensity)
+    G = g * np.exp(log_out_intensity)
+    B = b * np.exp(log_out_intensity)
+    return R, G, B
+
+
+def reinhards_global(E, a=0.18):
+    delta = 0.0001
+    x = E.shape[0]
+    y = E.shape[1]
+    L_w = np.exp(np.sum(np.log(delta + E)) / (x*y))
+    L_xy = a * (E/L_w)
+    L_d = L_xy / (1 + L_xy) * 256
+    return L_d
 
 
 def make_hdr(images, times):
@@ -140,19 +163,29 @@ def make_hdr(images, times):
     index_x = np.random.randint(0, x, n_samples)
     index_y = np.random.randint(0, y, n_samples)
     gs = []
+    Es = []
+    tone_mapping = reinhards_global
     for i in range(colors):
         subimgs = [im[:, :, i] for im in images]
+        print("recover g")
         g = recover_g(subimgs, times, index_x, index_y)
         gs.append(g)
-        lnE = radiance(g, subimgs, times, w)
-        E = np.exp(lnE)
+        print("radiance")
+        E = radiance(g, subimgs, times, w)
+        print("tone mapping")
         p = tone_mapping(E)
+        # Es.append(p)
+
         subimg = Image.fromarray(np.array(p, dtype=np.int8), mode='L')
         subs.append(subimg)
-
+    # R, G, B = local_tone_mapping(Es[0], Es[1], Es[2])
+    # img_R = Image.fromarray(np.array(R, dtype=np.int8), mode='L')
+    # img_G = Image.fromarray(np.array(G, dtype=np.int8), mode='L')
+    # img_B = Image.fromarray(np.array(B, dtype=np.int8), mode='L')
     x = np.arange(0, 256)
     for g in gs:
         plt.plot(g, x)
     plt.show()
     img = Image.merge('RGB', (subs[0], subs[1], subs[2]))
+    # img = Image.merge('RGB', (img_R, img_G, img_B))
     return img
